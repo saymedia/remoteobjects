ac559ff8a3925b0d12e013233237be816eeda7b3
Mark Paschal <mark@sixapart.com>
2009-04-20 22:00:47 +0000

    Add package metadata

--- __init__.py
+++ __init__.py
@@ -76,6 +76,13 @@ but extensible and replaceable through plain old subclassing.
 
 """
 
+__version__ = '1.0'
+__date__ = '20 April 2009'
+__author__ = 'Six Apart, Ltd.'
+__credits__ = """Brad Choate
+Leah Culver
+Mark Paschal"""
+
 import remoteobjects.dataobject
 import remoteobjects.fields as fields
 import remoteobjects.http
