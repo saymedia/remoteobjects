aa15dbe538ead2b2bf4448eea90408f94c9b5c4f
Brad Choate <brad@bradchoate.com>
2009-04-22 22:58:07 +0000

    Removed extra ','.

--- __init__.py
+++ __init__.py
@@ -78,7 +78,7 @@ but extensible and replaceable through plain old subclassing.
 
 __version__ = '1.0'
 __date__ = '20 April 2009'
-__author__ = 'Six Apart, Ltd.'
+__author__ = 'Six Apart Ltd.'
 __credits__ = """Brad Choate
 Leah Culver
 Mark Paschal"""
