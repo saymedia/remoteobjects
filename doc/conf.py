ee85ab7c0aaec5fa0000e260dcbf2f5d2ba6b57f
Mark Paschal <mark@sixapart.com>
2009-08-14 20:09:51 +0000

    Autodocument __init__ methods

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'document_init_methods']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -196,6 +196,3 @@ latex_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
-
-# For an autoclass, concatenate in its __init__'s docstring
-autoclass_content = "both"
