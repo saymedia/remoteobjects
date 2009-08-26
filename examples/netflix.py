a52a75b4c0afa255dc8bdb0ecfac94727fcc48e1
Mark Paschal <mark@sixapart.com>
2009-08-26 11:10:09 -0700

    Do a search for this Netflix example

--- examples/netflix.py
+++ examples/netflix.py
@@ -11,38 +11,55 @@ __date__ = '25 August 2009'
 __author__ = 'Mark Paschal'
 
 
+import cgi
+from optparse import OptionParser
 import sys
 from urllib import urlencode
-from urlparse import urljoin, urlparse, urlunparse
+import urlparse
 from xml.etree import ElementTree
 
-from oauth import OAuthConsumer
-from oauthclient import NetflixHttp
+import httplib2
+from oauth.oauth import OAuthConsumer, OAuthRequest, OAuthSignatureMethod_HMAC_SHA1
 
-from remoteobjects import RemoteObject, fields
+from remoteobjects import RemoteObject, fields, PageObject
 
 
 class Flixject(RemoteObject):
 
     content_types = ('text/xml', 'application/xml')
+    api_token = None
 
-    @classmethod
-    def http(cls, access_token=None):
-        h = NetflixHttp()
-        h.consumer = OAuthConsumer(*settings.NETFLIX_KEY)
-        h.add_credentials(h.consumer, access_token, domain="api.netflix.com")
-        return h
+    def get_request(self, headers=None, **kwargs):
+        request = super(Flixject, self).get_request(headers=headers, **kwargs)
+        method = request.get('method', 'GET')
 
-    @classmethod
-    def get(cls, url, **kwargs):
-        if not urlparse(url)[1]:
-            url = urljoin('https://api.netflix.com/', url)
+        # Apply OAuthness.
+        csr = OAuthConsumer(*self.api_token)
+        orq = OAuthRequest.from_consumer_and_token(csr, http_method=method,
+            http_url=request['uri'])
 
-        return super(Flixject, cls).get(url, http=cls.http(), **kwargs)
+        # OAuthRequest will strip our query parameters, so add them back in.
+        parts = list(urlparse.urlparse(self._location))
+        queryargs = cgi.parse_qs(parts[4], keep_blank_values=True)
+        for key, value in queryargs.iteritems():
+            orq.set_parameter(key, value[0])
+
+        # Sign the request.
+        osm = OAuthSignatureMethod_HMAC_SHA1()
+        orq.set_parameter('oauth_signature_method', osm.get_name())
+        orq.sign_request(osm, csr, None)
+
+        if method == 'GET':
+            request['uri'] = orq.to_url()
+        else:
+            request['headers'].update(orq.to_header())
+
+        return request
 
     def update_from_tree(self, tree):
         data = dict((k, v(tree)) for k, v in self.decoder_ring.items())
         self.update_from_dict(data)
+        return self
 
     def update_from_response(self, url, response, content):
         self.raise_for_response(url, response, content)
@@ -57,6 +74,7 @@ class Title(Flixject):
     title   = fields.Field()
     link    = fields.Field()
     thumb   = fields.Field()
+    #synopsis = fields.Link(...)
 
     decoder_ring = {
         'title': lambda x: x.find('title').get('regular'),
@@ -64,6 +82,44 @@ class Title(Flixject):
         'thumb': lambda x: x.find('box_art').get('large'),
         'api_url': lambda x: x.find('id'),
     }
+
+
+class Catalog(Flixject):
+
+    results = fields.List(fields.Field())
+    total   = fields.Field()
+    offset  = fields.Field()
+    limit   = fields.Field()
+
+    decoder_ring = {
+        'results': lambda x: [Title().update_from_tree(tree) for tree in x.findall('catalog_title')],
+        'total':   lambda x: int(x.find('number_of_results').text),
+        'offset':  lambda x: int(x.find('start_index').text),
+        'limit':   lambda x: int(x.find('results_per_page').text),
+    }
+
+
+def do_search(opts, args):
+    query = ' '.join(args)
+
+    search = Catalog.get('http://api.netflix.com/catalog/titles').filter(term=query)
+    search.deliver()
+
+    if len(search.results) == 0:
+        print "No results for %r" % query
+    elif len(search.results) == 1:
+        result = search.results[0]
+        print "## %s ##" % result.title
+    else:
+        print "## Results for %r ##" % query
+        print
+        for title in search.results:
+            if title is None:
+                print "(oops, none)"
+            else:
+                print title.title
+
+    return 0
 
 
 def main(argv=None):
@@ -75,13 +131,18 @@ def main(argv=None):
         help="Netflix API key (required)")
     parser.add_option("-s", "--secret", dest="secret",
         help="Netflix API shared secret (required)")
+    parser.add_option("--search", action="store_const", const=do_search,
+        dest="action", default=do_search,
+        help="Search for an item by title (default)")
     opts, args = parser.parse_args()
 
     if opts.key is None or opts.secret is None:
         print >>sys.stderr, "Options --key and --secret are required"
         return 1
 
-    return 0
+    Flixject.api_token = (opts.key, opts.secret)
+
+    return opts.action(opts, args)
 
 
 if __name__ == '__main__':
