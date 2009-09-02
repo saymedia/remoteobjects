743880c552a35b138c88e9a5e1bbc211cd6daa91
Mark Paschal <markpasc@markpasc.org>
2009-09-02 11:39:11 -0700

    Add github-tools to Sphinx config for building to github pages

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ sys.path.append(os.path.abspath('exts'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'document_init_methods']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'github.tools.sphinx', 'document_init_methods']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
