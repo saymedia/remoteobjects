94cdd09a9836a95ca44c42517bddc747ba0ea168
Mark Paschal <markpasc@markpasc.org>
2009-09-21 11:20:29 -0700

    Use nose's decorator wrapper instead of ad-hoc'ing one here

--- tests/utils.py
+++ tests/utils.py
@@ -4,14 +4,11 @@ import os
 
 import mox
 import nose
-
-
-def update_wrapper(wrapper, original):
-    for field in ('__module__', '__name__', '__doc__'):
-        setattr(wrapper, field, getattr(original, field))
+import nose.tools
 
 
 def todo(fn):
+    @nose.tools.make_decorator(fn)
     def test_reverse(*args, **kwargs):
         try:
             fn(*args, **kwargs)
@@ -19,7 +16,6 @@ def todo(fn):
             pass
         else:
             raise AssertionError('test %s unexpectedly succeeded' % fn.__name__)
-    update_wrapper(test_reverse, fn)
     return test_reverse
 
 
